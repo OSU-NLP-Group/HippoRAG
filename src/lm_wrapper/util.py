dd48b00bd32c18bc8574ae801e2ddd3b3dab185f
Yiheng Shu <shuyiheng29@163.com>
2024-06-28 20:06:48 -0400

    Fix colbertv2

--- src/lm_wrapper/util.py
+++ src/lm_wrapper/util.py
@@ -1,12 +1,7 @@
-from src.lm_wrapper.colbertv2_util import ColBERTv2Wrapper
-from src.lm_wrapper.gritlm import GritWrapper
-from src.lm_wrapper.huggingface_util import HuggingFaceWrapper
-
-
 def init_embedding_model(model_name):
     if 'GritLM/' in model_name:
+        from src.lm_wrapper.gritlm import GritWrapper
         return GritWrapper(model_name)
     elif model_name not in ['colbertv2', 'bm25']:
+        from src.lm_wrapper.huggingface_util import HuggingFaceWrapper
         return HuggingFaceWrapper(model_name)  # HuggingFace model for retrieval
-    elif model_name == 'colbertv2':
-        return ColBERTv2Wrapper()
