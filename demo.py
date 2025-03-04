44dedc96a6701f46a9d78b598b1cfc3575d79ada
jimenezgutierrez.1 <jimenezgutierrez.1@cse-cnc196881s.coeit.osu.edu>
2025-03-04 19:37:58 +0000

    Updating demo.py and requirements.txt. Adding OpenAI embedding model option.

--- demo.py
+++ demo.py
@@ -4,30 +4,61 @@ import json
 import argparse
 import logging
 
-from hipporag import HippoRAG
+from src.hipporag import HippoRAG
 
 def main():
 
-    corpus_path = f"reproduce/dataset/sample_corpus.json"
-    with open(corpus_path, "r") as f:
-        corpus = json.load(f)
-
-    docs = [f"{doc['title']}\n{doc['text']}" for doc in corpus]
-
     # Prepare datasets and evaluation
-    samples = json.load(open(f"reproduce/dataset/sample.json", "r"))
-    all_queries = [s['question'] for s in samples]
+    docs = [
+        "Oliver Badman is a politician.",
+        "George Rankin is a politician.",
+        "Thomas Marwick is a politician.",
+        "Cinderella attended the royal ball.",
+        "The prince used the lost glass slipper to search the kingdom.",
+        "When the slipper fit perfectly, Cinderella was reunited with the prince.",
+        "Erik Hort's birthplace is Montebello.",
+        "Marina is bom in Minsk.",
+        "Montebello is a part of Rockland County."
+    ]
 
-    hipporag = HippoRAG(working_dir="default")
+    save_dir = 'outputs'  # Define save directory for HippoRAG objects (each LLM/Embedding model combination will create a new subdirectory)
+    llm_model_name = 'gpt-4o-mini'  # Any OpenAI model name
+    embedding_model_name = 'text-embedding-3-small'  # Embedding model name (NV-Embed, GritLM or Contriever for now)
 
+    # Startup a HippoRAG instance
+    hipporag = HippoRAG(save_dir=save_dir,
+                        llm_model_name=llm_model_name,
+                        embedding_model_name=embedding_model_name)
+
+    # Run indexing
     hipporag.index(docs=docs)
 
-    # Retrieval and QA
-    retrieval_results = hipporag.retrieve(queries=all_queries)
-    hipporag.qa(retrieval_results)
+    # Separate Retrieval & QA
+    queries = [
+        "What is George Rankin's occupation?",
+        "How did Cinderella reach her happy ending?",
+        "What county is Erik Hort's birthplace a part of?"
+    ]
 
-    hipporag.rag_qa(queries=retrieval_results)
-    hipporag.rag_qa(queries=all_queries)
+    # For Evaluation
+    answers = [
+        ["Politician"],
+        ["By going to the ball."],
+        ["Rockland County"]
+    ]
+
+    gold_docs = [
+        ["George Rankin is a politician."],
+        ["Cinderella attended the royal ball.",
+         "The prince used the lost glass slipper to search the kingdom.",
+         "When the slipper fit perfectly, Cinderella was reunited with the prince."],
+        ["Erik Hort's birthplace is Montebello.",
+         "Montebello is a part of Rockland County."]
+    ]
+
+    print(hipporag.rag_qa(queries=queries,
+                                  gold_docs=gold_docs,
+                                  gold_answers=answers))
 
 if __name__ == "__main__":
     main()
--- src/hipporag/embedding_model/__init__.py
+++ src/hipporag/embedding_model/__init__.py
@@ -2,6 +2,7 @@ from .Contriever import ContrieverModel
 from .base import EmbeddingConfig, BaseEmbeddingModel
 from .GritLM import GritLMEmbeddingModel
 from .NVEmbedV2 import NVEmbedV2EmbeddingModel
+from .OpenAI import OpenAIEmbeddingModel
 
 from ..utils.logging_utils import get_logger
 
@@ -15,4 +16,6 @@ def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2")
         return NVEmbedV2EmbeddingModel
     elif "contriever" in embedding_model_name:
         return ContrieverModel
+    elif "text-embedding" in embedding_model_name:
+        return OpenAIEmbeddingModel
     assert False, f"Unknown embedding model name: {embedding_model_name}"
